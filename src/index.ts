12e5ad90eb013454c2b137e0af19cc344b5b895a
nerojz <nerojz86@gmail.com>
2026-05-01 11:10:14 +0800

    Added CSV Reader + Sampling

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 
 import { ChatOpenAI } from '@langchain/openai';
+import path from 'node:path';
+import { csvToJson } from './utils';
 
 
 async function run() {
-  const model = new ChatOpenAI({
-    model: 'gpt-4o-mini'
-  });
+  // const model = new ChatOpenAI({
+  //   model: 'gpt-4o-mini'
+  // });
 
-  const res = await model.invoke(`Explain CSV schema inference in 3 lines`);
+  // const res = await model.invoke(`Explain CSV schema inference in 3 lines`);
 
-  console.log(res.content);
+  // console.log(res.content);
+
+  let filePath = path.join(process.cwd(), 'data', 'salaries_2023.csv');
+
+  const {
+    headers,
+    first,
+    last
+  }: { headers: any, first: any, last: any } = await csvToJson(filePath, 10);
+
+  console.log(headers);
+  console.log(first);
+  console.log(last);
 }
 
 
---
+++ src/utils.ts
@@ -0,0 +1,53 @@
+import csv from 'csv-parser';
+import fs from 'node:fs';
+
+type CsvRow = Record<string, string>;
+
+type Result = {
+  headers: string[];
+  first: CsvRow[];
+  last: CsvRow[];
+};
+
+/**
+ * Return headers, first-n and last-n records
+ * @param filePath - csv file path
+ * @param n Read first and last N records
+ * @returns Result
+ */
+export const csvToJson = async (
+  filePath: string,
+  n: number = 5
+): Promise<Result> => {
+  return new Promise((resolve, reject) => {
+    let readStream = fs.createReadStream(filePath);
+
+    let headers: any[] = [];
+    let firstNRows: any[] = [];
+    let lastNRows: any[] = [];
+
+    readStream
+      .pipe(csv())
+      .on('headers', (headerList) => {
+        headers = [...headerList];
+      })
+      .on('data', (row) => {
+        if (firstNRows.length < n) {
+          firstNRows.push(row);
+        }
+
+        lastNRows.push(row);
+        if (lastNRows.length > n) {
+          lastNRows.shift();
+        }
+      })
+      .on('end', () => {
+        resolve({
+          headers,
+          first: firstNRows,
+          last: lastNRows,
+        });
+      })
+      .on('error', reject);
+  });
+};
